e09943dbb47376b3402f1c0662d0bf0ce98864d1
Hanss Espinoza <hanssabinadie@gmail.com>
2023-08-07 22:44:10 -0600

    First commit

---
+++ src/heroes/components/HeroList.tsx
@@ -0,0 +1,22 @@
+import { HeroCard } from "../components";
+import { getHeroesByPublisher } from "../helpers";
+import { Hero } from "../interfaces";
+
+interface HeroListProps {
+  publisher: string;
+}
+
+export const HeroList = ({publisher}: HeroListProps) => {
+
+  const heroes: Hero[] = getHeroesByPublisher(publisher);
+
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-3">
+      {
+        heroes.map(hero => (
+          <HeroCard key={hero.id} {...hero}/>
+        ))
+      }
+    </div>
+  )
+}
